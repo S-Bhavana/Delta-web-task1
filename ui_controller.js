358564ed7c6ff05a7958dc76a77d408f7044151c
S-Bhavana <bhavana.student2311@gmail.com>
2026-05-17 21:05:56 +0530

    Completed normal mode

--- ui_controller.js
+++ ui_controller.js
@@ -19,10 +19,7 @@ function renderBoard(){
                 cell.classList.add(cellData.owner);
             }
 
-            if(cellData.count > 0){
-
-                cell.innerText = cellData.count;
-            }
+            cell.innerText = cellData.count;
 
             cell.addEventListener("click",function(){
 
@@ -31,41 +28,6 @@ function renderBoard(){
 
             boardDiv.appendChild(cell);
         }
-    }
-
-    document.getElementById("currentPlayer")
-    .innerText =
-
-    "Current Player : " +
-
-    players[currentPlayer].toUpperCase();
-
-    renderMoveHistory();
-}
-
-function renderMoveHistory(){
-
-    let historyList =
-
-    document.getElementById("moveHistory");
-
-    historyList.innerHTML = "";
-
-    for(
-
-        let i = moveHistory.length - 1;
-
-        i >= 0;
-
-        i--
-
-    ){
-
-        let li = document.createElement("li");
-
-        li.innerText = moveHistory[i];
-
-        historyList.appendChild(li);
     }
 }
 
