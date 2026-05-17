c08838f001ab7e66a43b8b5161b8c54da0f8468f
S-Bhavana <bhavana.student2311@gmail.com>
2026-05-17 19:17:46 +0530

    Update game_engine.js

--- game_engine.js
+++ game_engine.js
@@ -1,1 +1,189 @@
+function getCapacity(row,col){
 
+    let capacity = 0;
+
+    if(row > 0){
+
+        capacity++;
+    }
+
+    if(row < ROWS - 1){
+
+        capacity++;
+    }
+
+    if(col > 0){
+
+        capacity++;
+    }
+
+    if(col < COLS - 1){
+
+        capacity++;
+    }
+
+    return capacity;
+}
+
+function explode(row,col,player){
+
+    board[row][col].count = 0;
+
+    board[row][col].owner = null;
+
+    let directions = [
+
+        [-1,0],
+        [1,0],
+        [0,-1],
+        [0,1]
+    ];
+
+    for(let i = 0; i < directions.length; i++){
+
+        let newRow = row + directions[i][0];
+
+        let newCol = col + directions[i][1];
+
+        if(
+
+            newRow >= 0 &&
+            newRow < ROWS &&
+            newCol >= 0 &&
+            newCol < COLS
+
+        ){
+
+            board[newRow][newCol].count++;
+
+            board[newRow][newCol].owner = player;
+
+            if(
+
+                board[newRow][newCol].count >=
+                getCapacity(newRow,newCol)
+
+            ){
+
+                explode(newRow,newCol,player);
+            }
+        }
+    }
+}
+
+function placeOrb(row,col){
+
+    if(gamePaused || gameOver){
+
+        return;
+    }
+
+    let cell = board[row][col];
+
+    let player = players[currentPlayer];
+
+    if(
+
+        cell.owner !== null &&
+        cell.owner !== player
+
+    ){
+
+        return;
+    }
+
+    cell.count++;
+
+    cell.owner = player;
+
+    moveHistory.push(
+
+        "Move " +
+        moveNumber +
+        " : " +
+        player.toUpperCase() +
+        " -> (" +
+        row +
+        "," +
+        col +
+        ")"
+
+    );
+
+    moveNumber++;
+
+    if(
+
+        cell.count >= getCapacity(row,col)
+
+    ){
+
+        explode(row,col,player);
+    }
+
+    renderBoard();
+
+    checkWinner();
+
+    if(!gameOver){
+
+        currentPlayer++;
+
+        currentPlayer %= players.length;
+
+        turnTime = 15;
+
+        renderBoard();
+    }
+}
+
+function checkWinner(){
+
+    let redExists = false;
+
+    let blueExists = false;
+
+    for(let row = 0; row < ROWS; row++){
+
+        for(let col = 0; col < COLS; col++){
+
+            if(board[row][col].owner === "red"){
+
+                redExists = true;
+            }
+
+            if(board[row][col].owner === "blue"){
+
+                blueExists = true;
+            }
+        }
+    }
+
+    /* IMPORTANT FIX */
+
+    if(moveNumber <= players.length + 1){
+
+        return;
+    }
+
+    if(redExists && !blueExists){
+
+        gameOver = true;
+
+        alert("RED WINS");
+    }
+
+    if(blueExists && !redExists){
+
+        gameOver = true;
+
+        alert("BLUE WINS");
+    }
+
+    if(gameTime <= 0){
+
+        gameOver = true;
+
+        alert("TIME OVER");
+    }
+}
